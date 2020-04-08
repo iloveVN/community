48ed62e9c477f88ff65d11ec3c9ea9588076bfc6
iloveVN <1743355246@qq.com>
2020-04-08 22:57:14 +0800

    修改redis源为阿里云服务器

--- src/main/java/com/garen/community/domain/JpaUser.java
+++ src/main/java/com/garen/community/domain/JpaUser.java
@@ -1,16 +1,27 @@
 package com.garen.community.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
+/**
+ * 使用JPA注解配置映射关系
+ */
 @Data
-@Entity
-@Table(name = "tbl_user")
-public class JpaUser {
+@NoArgsConstructor
+@Entity  // 告诉JSP这是一个实体类(和数据库表映射的类)
+@Table(name = "tbl_user")  // 指定和哪个数据表对应, 如果省略，默认就是类名小写jpauser
+public class JpaUser implements Serializable {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 自增主键
     private Integer id;
+
+    @Column(length = 50) // 这是和数据表对应的一个列
     private String name;
+    @Column // 省略，默认列名就是属性名
     private String email;
+
 }
