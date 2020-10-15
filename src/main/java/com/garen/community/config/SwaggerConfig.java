8dcb07d539b7bf6cfa45c05fc34c34897b0717cd
lvgaoren <742869059@qq.com>
2020-10-15 09:26:37 +0800

    swagger配置修改

--- src/main/java/com/garen/community/config/SwaggerConfig.java
+++ src/main/java/com/garen/community/config/SwaggerConfig.java
@@ -11,6 +11,11 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ *  http://localhost:port/swagger-ui.html  // 接口文档访问地址
+ * @author 0283000196
+ *
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
